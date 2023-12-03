55ab59559e03f0ea2ae89380de1a0354354e3606
Tato Levicz <leviczios@gmail.com>
2023-12-03 11:19:00 -0300

    instructions

--- main.cpp
+++ main.cpp
@@ -1,11 +1,32 @@
-#include "mainwindow.h"
+//#include "mainwindow.h"
+//
+//#include <QApplication>
+//
+//int main(int argc, char *argv[])
+//{
+//    QApplication a(argc, argv);
+//    MainWindow w;
+//    w.show();
+//    return a.exec();
+//}
 
-#include <QApplication>
 
-int main(int argc, char *argv[])
-{
-    QApplication a(argc, argv);
-    MainWindow w;
-    w.show();
-    return a.exec();
+#include <sol/sol.hpp>
+#include <cassert>
+
+int main() {
+
+    sol::state lua;
+    int x = 0;
+
+    lua.set_function("beep", [&x]{
+        ++x;
+        std::cout << "beep: " << x << std::endl;
+    });
+
+    lua.script("beep()");
+    lua.script("beep()");
+    lua.script("beep()");
+    
+    assert(x == 3);
 }
