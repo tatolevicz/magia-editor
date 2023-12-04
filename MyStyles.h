89884903fb91de7f3c75fb19fa6a7f4066ca013b
Tato Levicz <leviczios@gmail.com>
2023-12-03 23:18:24 -0300

    refactor ids

--- MyStyles.h
+++ MyStyles.h
@@ -7,6 +7,19 @@
 //#include ""
 namespace mystyles {
 
+
+    struct Margins {
+        inline static int SYMBOLS = 0;
+        inline static int NUMBERS = 1;
+        inline static int FOLDING = 2;
+    };
+
+    struct Markers {
+        inline static int ERROR = 1;
+        //todo continue adding
+        inline static int OTHERS = 2;
+    };
+
     struct LuaEditorColors {
         inline static int PRE_PROC = 0xDDB26D; // Funções e métodos
         inline static int IDENTIFIER = 0xE7BEB4; // Identificadores
@@ -23,48 +36,17 @@ namespace mystyles {
         inline static int OPEN_STRING = 0xE7BEB4; // Strings
         inline static int OPERATOR = 0xE7BEB4; // Operadores
         inline static int CONSTANT = 0xAAB362; // Constantes (nil, true, false)
-
     };
-
-//    struct Colors {
-//        inline static int WHITE = 0xFFFFFF;
-//        inline static int GREY = 0x808080;
-//        inline static int DARK_GREY = 0x404040;
-//        inline static int RED = 0x0000FF;
-//        inline static int GREEN = 0x00FF00;
-//        inline static int BLUE = 0xFF0000;
-//        inline static int YELLOW = 0x00FFFF;
-//        inline static int CYAN = 0xFFFF00;
-//        inline static int MAGENTA = 0xFF00FF;
-//        inline static int ORANGE = 0x00A5FF;
-//        inline static int BROWN = 0x2A2AA5;
-//        inline static int PINK = 0xCBC0FF;
-//        inline static int PURPLE = 0x800080;
-//        inline static int LIME = 0x00FF00;
-//        inline static int OLIVE = 0x008080;
-//        inline static int TEAL = 0x808000;
-//        inline static int NAVY = 0x800000;
-//        inline static int MAROON = 0x000080;
-//        inline static int CORAL = 0x507FFF;
-//        inline static int KHAKI = 0x8CE6F0;
-//        inline static int VIOLET = 0xEE82EE;
-//        inline static int LIGHT_GREY = 0xD3D3D3;
-//    };
-//
-//
-
 
    struct editor{
        inline static void setupFolding(ScintillaEdit* editor) {
            // Configurar a margin para dobradura (número da margin, geralmente 2 para dobradura)
-           int marginId = 2;
            int marginWidth = 16; // Largura da margin em pixels
 
-           editor->send(SCI_SETMARGINWIDTHN, marginId, marginWidth);
-           editor->send(SCI_SETMARGINMASKN, marginId, SC_MASK_FOLDERS);
-           editor->send(SCI_SETMARGINTYPEN, marginId, SC_MARGIN_SYMBOL);
-           editor->send(SCI_SETMARGINSENSITIVEN, marginId, true);
-
+           editor->send(SCI_SETMARGINWIDTHN, Margins::FOLDING, marginWidth);
+           editor->send(SCI_SETMARGINMASKN,  Margins::FOLDING, SC_MASK_FOLDERS);
+           editor->send(SCI_SETMARGINTYPEN,  Margins::FOLDING, SC_MARGIN_SYMBOL);
+           editor->send(SCI_SETMARGINSENSITIVEN,  Margins::FOLDING, true);
 
            editor->send(SCI_MARKERDEFINE, SC_MARKNUM_FOLDEROPEN, SC_MARK_BOXMINUS);
            editor->send(SCI_MARKERDEFINE, SC_MARKNUM_FOLDER, SC_MARK_BOXPLUS);
@@ -73,8 +55,6 @@ namespace mystyles {
            editor->send(SCI_MARKERDEFINE, SC_MARKNUM_FOLDEREND, SC_MARK_BOXPLUSCONNECTED);
            editor->send(SCI_MARKERDEFINE, SC_MARKNUM_FOLDEROPENMID, SC_MARK_BOXMINUSCONNECTED);
            editor->send(SCI_MARKERDEFINE, SC_MARKNUM_FOLDERMIDTAIL, SC_MARK_TCORNER);
-
-
 
            editor->send(SCI_MARKERSETBACK, SC_MARKNUM_FOLDER, LuaEditorColors::LINE_ACIVE);
            editor->send(SCI_MARKERSETBACK, SC_MARKNUM_FOLDEROPEN, LuaEditorColors::LINE_ACIVE);
@@ -108,32 +88,42 @@ namespace mystyles {
             editor->styleSetSize(STYLE_DEFAULT, 16);
             editor->styleClearAll(); //this copies the defualt style to all the others
 
+            editor->send(SCI_SETTABWIDTH, 2);
+            editor->send(SCI_SETUSETABS, 1); // Use 1 para usar tabs reais
+
+
+            // Habilita a visualização das guias de indentação
+//            editor->send(SCI_SETINDENTATIONGUIDES, SC_IV_LOOKBOTH);
+
+// Ativa a autoindentação
+//            editor->send(SCI_SETAUTOINDENT, 1);
+
+
             editor->setCaretFore(LuaEditorColors::IDENTIFIER);
             editor->setCaretLineBack(LuaEditorColors::LINE_ACIVE);  // Define a cor de fundo para o destaque
-            editor->setCaretLineFrame(1.0);
+            editor->setCaretLineFrame(1);
+
 
             editor->styleSetBack(STYLE_LINENUMBER, LuaEditorColors::BACKGROUND); // Cinza para fundo
             editor->styleSetFore(STYLE_LINENUMBER, LuaEditorColors::COMMENT); // Branco para texto
-//            editor->styleSetFont(STYLE_LINENUMBER, "Courier New");
-//            editor->styleSetFont(STYLE_LINENUMBER, "Poppins");
             editor->styleSetSize(STYLE_LINENUMBER, 12);
 
-            editor->setMarginTypeN(1, SC_MARGIN_NUMBER);
-            editor->setMarginWidthN(1,25);
-            editor->setMarginMaskN(1, 0); // nao permite a renderizacao de marcadores na margem 1
+            editor->setMarginTypeN(Margins::NUMBERS, SC_MARGIN_NUMBER);
+            editor->setMarginWidthN(Margins::NUMBERS,25);
+            editor->setMarginMaskN(Margins::NUMBERS, 0); // nao permite a renderizacao de marcadores na margem 1
 
-            editor->setMarginWidthN(0,30);
-            editor->setMarginTypeN(0, SC_MARGIN_COLOUR);
-            editor->setMarginMaskN(0, 1 << 1); // Permite o marcador 1 na margem 0
+            editor->setMarginWidthN(Margins::SYMBOLS,30);
+            editor->setMarginTypeN(Margins::SYMBOLS, SC_MARGIN_COLOUR);
+            editor->setMarginMaskN(Margins::SYMBOLS, 1 << Markers::ERROR); // Permite o marcador de error na margem 1
 
             setupFolding(editor);
-//
+
             int markerBreakpoint = 1; // Escolha um número de índice para o marcador de breakpoint
             editor->markerDefine(markerBreakpoint, SC_MARK_CIRCLE);
-            editor->markerSetFore(markerBreakpoint, LuaEditorColors::ERRORS); // Cor azul para o símbolo
-            editor->markerSetBack(markerBreakpoint, LuaEditorColors::ERRORS); //
+            editor->markerSetFore(markerBreakpoint, LuaEditorColors::ERRORS);
+            editor->markerSetBack(markerBreakpoint, LuaEditorColors::ERRORS);
 
-            editor->setMarginBackN(0, LuaEditorColors::BACKGROUND);
+            editor->setMarginBackN(Margins::SYMBOLS, LuaEditorColors::BACKGROUND);
         }
    };
 
